72ef397d1c9af868016307496c1e0d16ab2f9dbf
Marco Aurélio Silva de Souza Júnior <aureliojuniorcmrj@hotmail.com>
2021-08-25 18:49:25 -0300

    versão 1 da Classe Fabricante

--- codes/src/ProdutorConsumidor/Fabricante.java
+++ codes/src/ProdutorConsumidor/Fabricante.java
@@ -1,20 +1,32 @@
 package ProdutorConsumidor;// import java.util.Thread;
 
+import java.util.concurrent.Semaphore;
+
 public class Fabricante extends Thread {
-    
+    private String nome;
+    private Semaphore askIfCanProduce;
+    private Semaphore sayItIsProduced;
+    private FilaVenda filaVenda;
 
-    public Fabricante() {
 
+    public Fabricante(String nome, Semaphore semaphore1, 
+    Semaphore semaphore2, FilaVenda filaVenda) {
+        this.nome = nome;
+        this.askIfCanProduce = semaphore1;
+        this.sayItIsProduced = semaphore2;
+        this.filaVenda = filaVenda;
     }
 
     public void run() {
         try {
             while(true) {
                 Thread.sleep(2000);
-                FilaVenda.retiraVendaDaFila();
+                askIfCanProduce.acquire();
+                    // fabricante remove compra da fila
+                    filaVenda.retiraVendaDaFila();
+                sayItIsProduced.release();
             }
 
         } catch(Exception e) {e.printStackTrace();}
     }
-    // fabricante remove compra da fila
 }
