6b781d60eee99122aaed282ae599a19fe4cd14c2
Marco Aurélio Silva de Souza Júnior <aureliojuniorcmrj@hotmail.com>
2021-08-25 19:34:19 -0300

    atualização segundo versão 2
    - inserção de 4 semáforos;
    - duas propriedades novas: FilaVenda e FilaEntrega;
    - em run(): retira venda da filaVenda e insere entrega na filaEntrega.

--- codes/src/ProdutorConsumidor/Fabricante.java
+++ codes/src/ProdutorConsumidor/Fabricante.java
@@ -4,27 +4,42 @@ import java.util.concurrent.Semaphore;
 
 public class Fabricante extends Thread {
     private String nome;
+    private Semaphore askIfTheresSale;
+    private Semaphore saleIsBeingProduced;
     private Semaphore askIfCanProduce;
-    private Semaphore sayItIsProduced;
+    private Semaphore sayItsProduced;
     private FilaVenda filaVenda;
+    private FilaEntrega filaEntrega;
 
 
-    public Fabricante(String nome, Semaphore semaphore1, 
-    Semaphore semaphore2, FilaVenda filaVenda) {
+    public Fabricante(String nome, FilaVenda filaVenda, FilaEntrega filaEntrega,
+    Semaphore semaphore1, Semaphore semaphore2, 
+    Semaphore semaphore3, Semaphore semaphore4) { 
         this.nome = nome;
-        this.askIfCanProduce = semaphore1;
-        this.sayItIsProduced = semaphore2;
         this.filaVenda = filaVenda;
+        this.filaEntrega = filaEntrega;
+        this.askIfTheresSale = semaphore1;
+        this.saleIsBeingProduced = semaphore2;
+        this.askIfCanProduce = semaphore3;
+        this.sayItsProduced = semaphore4;
+
     }
+
+    // ver uma venda e criar uma entrega
+    
 
     public void run() {
         try {
             while(true) {
                 Thread.sleep(2000);
+                askIfTheresSale.acquire();
                 askIfCanProduce.acquire();
                     // fabricante remove compra da fila
-                    filaVenda.retiraVendaDaFila();
-                sayItIsProduced.release();
+                    Entrega entrega = filaVenda.retiraVendaDaFila();
+                    filaEntrega.insereEntregaNaFila(entrega);
+                saleIsBeingProduced.release();
+                sayItsProduced.release();
+
             }
 
         } catch(Exception e) {e.printStackTrace();}
